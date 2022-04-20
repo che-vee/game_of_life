f34edfe8293a4593d8c3d6cb903f41fce2499f6b
Valentine <VEE.LANGAT@GMAIL.COM>
2022-04-20 13:08:24 +0200

    implement oop

--- game_of_life.py
+++ game_of_life.py
@@ -1,42 +1,54 @@
 import numpy as np
 
-grid = np.array([
-          [0, 0, 0, 0, 0],
-          [0, 0, 1, 0, 0],
-          [1, 0, 1, 0, 0],
-          [0, 1, 1, 0, 0],
-          [0, 0, 0, 0, 0]
-      ])
 
-updated_grid = np.copy(grid)
+class Playground:
+    def __init__(self, grid):
+        self.grid = grid
 
-def update_status(updated_grid):
-    height = len(grid)
-    width = len(grid[0])
+    def step(self):
+        updated_grid = self.grid.copy()
+        height, width = self.grid.shape
 
-    for row_index in range(0, width):
-        for cell_index in range(0, height):
-            updated_grid[row_index][cell_index] = make_step(row_index, cell_index, grid)
+        for row_index in range(height):
+            for col_index in range(width):
+                updated_grid[row_index, col_index] = self.step_one_cell(
+                    row_index, col_index
+                )
 
-    return updated_grid
+        self.grid = updated_grid
 
+    def step_one_cell(self, row, col):
+        neighbour_count = (
+            np.sum(self.grid[row - 1 : row + 2, col - 1 : col + 2])
+            - self.grid[row, col]
+        )
 
-def make_step(row, cell, grid):
-    neighbour_count = np.sum(grid[row - 1:row + 2, cell - 1:cell + 2]) - grid[row, cell]
-
- 
-    if neighbour_count <= 1:
-        return 0
-    elif neighbour_count <= 3 and grid[row][cell] == 1:
-        return 1
-    elif neighbour_count == 3 and grid[row][cell] == 0:
-        return 1
-    else:
-        return 0
-    
-    
-print(grid)   
-print(update_status(updated_grid))
+        if neighbour_count <= 1:
+            return 0
+        elif neighbour_count <= 3 and self.grid[row, col] == 1:
+            return 1
+        elif neighbour_count == 3 and self.grid[row, col] == 0:
+            return 1
+        else:
+            return 0
 
 
+if __name__ == "__main__":
 
+    playground = Playground(
+        np.array(
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [1, 0, 1, 0, 0],
+                [0, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+            ]
+        )
+    )
+
+    for step in range(10):
+        print(playground.grid, end="\n-------------\n")
+        playground.step()
+
+    print(playground.grid, end="\n-------------\n")
